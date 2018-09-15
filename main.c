4af198878807662e73adc42e0332a306145b5d1c
Caíque <40068062+apieceofCAKE@users.noreply.github.com>
2018-09-15 15:13:19 -0300

    Update main.c
    Formatting changes.

--- main.c
+++ main.c
@@ -12,19 +12,91 @@ http://mathforum.org/dr.math/faq/faq.calendar.html
 https://support.microsoft.com/en-us/help/214019/method-to-determine-whether-a-year-is-a-leap-year
 */
 
+
+//Declaration
+int weekday(int day, int month, int year, int century); 
+int leap_year(int year, int century);
+
+
+//Main function
+int main() {
+
+    int d, m, y, c, wd, lp;
+
+    char *days[7];
+    days[0] = "Sunday";
+    days[1] = "Monday";
+    days[2] = "Tuesday";
+    days[3] = "Wednesday";
+    days[4] = "Thursday";
+    days[5] = "Friday";
+    days[6] = "Saturday";
+
+    printf("\nDay of the week finder. Enter a date.\n\n");
+    
+    printf("Day (1 to 31): \n");
+    scanf("%d", &d);
+
+    printf("\nMonth (1 to 12): \n");
+    scanf("%d", &m);
+
+    printf("\nYear (1 to 99): \n");
+    scanf("%d", &y);
+
+    printf("\nCentury (1 to 99): \n");
+    scanf("%d", &c);
+
+    printf(" \n");
+    printf(" \n");
+
+    lp = leap_year(y, c);
+
+    //Checking whether the year is a leap year and if the date is valid
+    if(lp == 1) {
+
+        printf("This year is a leap year.\n");
+
+        wd = weekday(d, m, y, c);
+
+        //Date standard used in Brazil (d/m/y)
+        printf("%d/%d/%d%d is %s.\n", d, m, c, y, days[wd]);
+    }
+
+    else {
+
+        if(d == 31 && m == 2) {
+
+            printf("The date is invalid because this isn't a leap year.\n");
+        }
+
+        else {
+
+            printf("This year isn't a leap year.\n");
+
+            wd = weekday(d, m, y, c);
+
+            printf("%d/%d/%d%d is %s.\n", d, m, c, y, days[wd]);
+        }
+    }
+
+    return 0;
+}
+
+
+//Definition
+int weekday(int day, int month, int year, int century) {
 //Returns a number between 1 and 7 where 1 stands for Sunday, 2 for Monday, 7 for Saturday and so on
 //Format (1-31,1-12,1-99,1-99)
-int weekday(int day, int month, int year, int century) {
-
+    
     int aux1;
     int aux2;
     int wd;
 
     //Normalizing the input to be suitable for the formula
-    if (month == 1) {
+    if(month == 1) {
 
         month = 11;
-        if (year == 00) {
+        if(year == 00) {
 
             year = 99;
             century --;
@@ -36,10 +108,10 @@ int weekday(int day, int month, int year, int century) {
         }
     }
 
-    else if (month == 2) {
+    else if(month == 2) {
 
         month = 12;
-        if (year == 00) {
+        if(year == 00) {
 
             year = 99;
             century --;
@@ -59,11 +131,11 @@ int weekday(int day, int month, int year, int century) {
     //Aplying the formula
     aux1 = day + ((13*month - 1)/5) + year + (year/4) + (century/4) - 2*century;
 
-    if (aux1 < 0) {
+    if(aux1 < 0) {
 
         aux2 = aux1;
 
-        while (aux2 % 7 != 0) {
+        while(aux2 % 7 != 0) {
 
             aux2 --;
         }
@@ -80,15 +152,15 @@ int weekday(int day, int month, int year, int century) {
 }
 
 
+int leap_year(int year, int century) {
 //Returns 1 if a year is a leap year and 0 if it isn't
 //Format (1-99,1-99)
-int leap_year(int year, int century) {
+    
+    if(year%4 == 0) {
 
-    if (year%4 == 0) {
+        if(year == 0) {
 
-        if (year == 0) {
-
-            if (century%4 == 0) {
+            if(century%4 == 0) {
 
                 return 1;
             }
@@ -109,81 +181,4 @@ int leap_year(int year, int century) {
 
         return 0;
     }
-}
-
-//Main function
-int main() {
-
-    int d;
-    int m;
-    int y;
-    int c;
-    int wd;
-    int lp;
-
-    char *days[7];
-    days[0] = "Sunday";
-    days[1] = "Monday";
-    days[2] = "Tuesday";
-    days[3] = "Wednesday";
-    days[4] = "Thursday";
-    days[5] = "Friday";
-    days[6] = "Saturday";
-
-    printf("Day of the week finder. Enter a date.\n");
-    printf(" \n");
-    printf(" \n");
-    printf("Day (1 to 31): \n");
-
-    scanf("%d", &d);
-
-    printf(" \n");
-    printf("Month (1 to 12): \n");
-
-    scanf("%d", &m);
-
-    printf(" \n");
-    printf("Year (1 to 99): \n");
-
-    scanf("%d", &y);
-
-    printf(" \n");
-    printf("Century (1 to 99): \n");
-
-    scanf("%d", &c);
-
-    printf(" \n");
-    printf(" \n");
-
-    lp = leap_year(y, c);
-
-    //Checking whether the year is a leap year and if the date is valid
-    if (lp == 1) {
-
-        printf("This year is a leap year.\n");
-
-        wd = weekday(d, m, y, c);
-
-        //Date standard used in Brazil (d/m/y)
-        printf("%d/%d/%d%d is %s.", d, m, c, y, days[wd]);
-    }
-
-    else {
-
-        if (d == 31 && m == 2) {
-
-            printf("The date is invalid because this isn't a leap year.\n");
-        }
-
-        else {
-
-            printf("This year isn't a leap year.\n");
-
-            wd = weekday(d, m, y, c);
-
-            printf("%d/%d/%d%d is %s.", d, m, c, y, days[wd]);
-        }
-    }
-
-    return 0;
 }
